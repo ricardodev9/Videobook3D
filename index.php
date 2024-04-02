ff00167ddb7ccfbc34ab903ccb636e606778bf74
ricardodev9 <ricardoapazacueva2000@gmail.com>
2024-04-03 01:06:53 +0200

    update en el homepage
    Se ha actualizado la fuente del homepage, así como la cabecera y una imagen de fondo

--- index.php
+++ index.php
@@ -5,44 +5,89 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VideoBook3D</title>
     <link rel="stylesheet" href="assets/css/styles.css">
+        <!-- tailwind -->
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- js del home -->
+    <script src="assets/js/home.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-
+    
     <div id="homeContent">
+   
 	<header class="headerContent">
 		<nav class="navContent">
-			<ul>
+			<ul class="flex items-center">
 				<li>
                 <!-- BUSCADOR -->                    
-          
-                    <label for="voice-search" class="sr-only">Search</label>
-                    <div class="relative w-full">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.15 5.6h.01m3.337 1.913h.01m-6.979 0h.01M5.541 11h.01M15 15h2.706a1.957 1.957 0 0 0 1.883-1.325A9 9 0 1 0 2.043 11.89 9.1 9.1 0 0 0 7.2 19.1a8.62 8.62 0 0 0 3.769.9A2.013 2.013 0 0 0 13 18v-.857A2.034 2.034 0 0 1 15 15Z"/>
-                            </svg>
-                        </div>
-                        <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
-                        <button type="button" class="absolute inset-y-0 end-0 flex items-center pe-3">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z"/>
-                            </svg>
+
+                    <div class="flex items-center">
+                        <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 ps-7 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Película,serie o libro" />
+                        <button type="submit" class="inline-flex items-center py-1.5 px-2 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>Buscar
                         </button>
                     </div>
-                    <button type="submit" class="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                        </svg>Buscar
-                    </button>
-           
+
 
 
                 </li>
-                <li>DESPLEGABLE CON OPCIONES DE PELICULA,SERIE O LIBRO</li>
-                <li>DESPLEGABLE CON OPCION DE ORDEN</li>
-                <li>ELIMINAR FILTROS X</li>
-                <li>FOTO DE PERFIL CON DESPLEGABLE</li>
+                <li>
+
+
+                <div class="select_nav" >
+                <label for="select_elemento">Categoría</label>&nbsp;&nbsp;
+                    <select id="select_elemento" class="btn btn-secondary dropdown-toggle">
+                       
+                        <option value="" selected>Ninguno</option>
+                        <option value="pelicula">Película</option>
+                        <option value="serie">Serie</option>
+                        <option value="libro">Libro</option>
+                    </select>
+                </div>
+
+
+
+                </li>
+
+
+                <li>
+                <div class="select_nav">
+                <label for="select_orden">Ordenar</label>&nbsp;&nbsp;
+                    <select id="select_orden" class="btn btn-secondary dropdown-toggle">       
+                        <option value="" selected>Ninguno</option>
+                        <option value="rating">Rating</option>
+                        <option value="duracion">Duración</option>
+                            
+                        </select>
+                    </div>
+                </li>
+
+                <!-- I am going to create a new dropdown with genres info -->
+                <li>
+                <div class="select_nav">
+
+                <label for="select_genero">Género</label>&nbsp;&nbsp;
+                    <select id="select_orden" class="btn btn-secondary dropdown-toggle">       
+                        <option value="" selected>Ninguno</option>
+
+                    </select>
+                </div>
+
+                </li>
+
+                <li>
+                <div class="select_nav">
+                    <span id="eliminar_filtro"> X </span>
+                </div>
+                </li>
+                <li>
+                    <img src="assets/img/perfil2.png" alt="perfil">
+                </li>
                 
 			</ul>
 		</nav>
@@ -68,6 +113,7 @@
 		
 	</section>
 
+    
     <aside>
 		<h2>About section</h2>
 		<p>Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
