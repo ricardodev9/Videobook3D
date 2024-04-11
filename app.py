8ce2241f60c9f670d0c39877e0dec9bef8e3ac07
ricardodev9 <164495350+ricardodev9@users.noreply.github.com>
2024-04-12 00:30:05 +0200

    getting info by id_imdb with api

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 #API para obtener informacón de los elementos: películas, series y libros
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+import requests
 import imdb
 import pprint
 import json
 import xmltodict
 from imdb import IMDbError
 import itertools
-app =  Flask(__name__)
+from flask_cors import CORS
 
+app =  Flask(__name__)
+CORS(app)  # Habilitar CORS para todos los endpoints
 # home route that returns below text when root url is accessed
 
-def search_element():
+def search_element(id):
     cine = imdb.IMDb()
-    #items = cine.search_movie("")
-    items = cine.search_movie('Avengers')
+    items = cine.get_movie(id)
+    #items = cine.search_movie(name)
+    
     return items
         
     #items_json = []
@@ -31,16 +35,43 @@ def search_element():
     #     }
     #     items_json.append(item_info)  
     #     print(item['title'])
-@app.route("/")    
+@app.route("/", methods=['POST'])    
 def show_element():
-    elements = search_element()
-    # Recorrer los elementos y obtener su información
-    for element in elements:
-        print("ID:", element.getID())  # Obtener el ID del elemento
-        print("Título:", element['title'])
-        print("Año:", element['year'])
-        print("Tipo:", element['kind'])
+    try:
+        ids_imdb = request.form.getlist('ids_imdb')
         
+        id='332280'
+        #elements = search_element(id)
+        results = []
+        for ids in ids_imdb:
+            results.append(ids)
+        
+        # Recorrer los elementos y obtener su información
+        
+        # for element in elements:
+        #     # print("ID:", element.getID())  # Obtener el ID del elemento
+        #     results.append({
+                
+        #         'Título': element['title'],
+        #         'año': element['year'],
+        #         'Tipo': element['kind']
+        #     })
+        
+        # results = {
+        #     'id' : id,
+        #     'titulo' : elements.get('title'),
+        #     'año' : elements.get('year'),
+        #     'tipo' : elements.get('kind')
+        # }
+        return jsonify({"message": results})    
+    except requests.RequestException as e:
+    # Manejar cualquier error de solicitud
+        return jsonify({
+            "success": False,
+            "message": f"Error al realizar la solicitud a la API de OpenWeatherMap: {str(e)}"
+        })    
+        
+    
     #     print()  # Imprimir una línea en blanco entre cada elemento
             
  
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,30 +1,46 @@
-// $(document).ready(function() {
-//     // Hacer una solicitud GET a la ruta /all de tu servidor Flask cuando la página se cargue
+/**
+ * when home is ready, we must 
+ */
+$(document).ready(function() {
+    // Hacer una solicitud GET a la ruta /all de tu servidor Flask cuando la página se cargue
+    var box_elementos = $('[id="id_imdb"]');
+    var array_ids = Array()
+    box_elementos.each(function(){
+        var imdb = $(this).val();
+        array_ids.push(imdb)
+    })
+
     
-//     $.ajax({
-//         url: 'http://localhost:5000/',
-//         method: 'GET',
-//         dataType: 'json',
-//         success: function(data) {
-//             // Manejar los datos recibidos
-//             if (data) {
-//                 var movieListDiv = $('#movieList');
-//                 $.each(data, function(index, movie) {
-//                     var movieInfo = `
-//                         <div>
-//                             <p>Título: ${movie.title}</p>
-//                             <p>Año: ${movie.year}</p>
-//                             <p>Tipo: ${movie.kind}</p>
-//                         </div>
-//                     `;
-//                     movieListDiv.append(movieInfo);
-//                 });
-//             } else {
-//                 console.error('Error: No se recibieron datos de las películas.');
-//             }
-//         },
-//         error: function(xhr, status, error) {
-//             console.error('Error al hacer la solicitud:', error);
-//         }
-//     });
-// });
+    var datos = {
+        'ids_imdb' : array_ids
+    }
+    console.log(datos);
+    $.ajax({
+        url: 'http://localhost:5000/',
+        type: "POST",
+        contentType: 'application/json',
+        data : datos,
+        success: function(response) {
+            console.log(response);
+            // // Manejar los datos recibidos
+            // if (data) {
+            //     var movieListDiv = $('#movieList');
+            //     $.each(data, function(index, movie) {
+            //         var movieInfo = `
+            //             <div>
+            //                 <p>Título: ${movie.title}</p>
+            //                 <p>Año: ${movie.year}</p>
+            //                 <p>Tipo: ${movie.kind}</p>
+            //             </div>
+            //         `;
+            //         movieListDiv.append(movieInfo);
+            //     });
+            // } else {
+            //     console.error('Error: No se recibieron datos de las películas.');
+            // }
+        },
+        error: function(xhr, status, error) {
+            console.error('Error al hacer la solicitud:', error);
+        }
+    });
+});
