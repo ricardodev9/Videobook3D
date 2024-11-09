95edee7e26a2a80c50420eee058eaa169f3839d1
ricardodev9 <164495350+ricardodev9@users.noreply.github.com>
2024-11-09 13:28:42 +0100

    Arcchivo para enlazar los demás archivos

---
+++ videobook3d.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  Incluye los archivos base para la plataforma
+
+*/
+// ini_set('display_errors', 0);  // No mostrar errores de PHP
+// error_reporting(0);            // Deshabilitar el reporte de errores de PHP
+require_once __DIR__.'/inc/class/Videobook.php';
+require_once __DIR__.'/inc/videobook3d/view.php';
+require_once __DIR__.'/inc/videobook3d/model.php';
+require_once __DIR__.'/inc/videobook3d/controller.php';
+require_once __DIR__.'/db_conn.php';
+?>
