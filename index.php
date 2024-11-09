01cef3db79f086aef232357bd45017e66657d64e
ricardodev9 <164495350+ricardodev9@users.noreply.github.com>
2024-11-09 13:31:27 +0100

    Ajuste y reorganización en el homepage

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-require_once 'conf.php';
+require 'videobook3d.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,7 +17,7 @@ require_once 'conf.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    
+
 </head>
 
 <body>
@@ -27,33 +26,16 @@ require_once 'conf.php';
     include 'resources/navbar.php';
     ?>
     <div id="homeContent">
-        
         <section>
             <?php
-        
-            // Conexión al servidor MySQL sin especificar una base de datos
-            $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, '', $DB_PORT);
-
             // Verificar conexión
             if ($conn->connect_error) {
                 die("Connection to MySQL server failed: " . $conn->connect_error);
             }
-
-            // Verificar si la base de datos existe
-            $result = $conn->query("SHOW DATABASES LIKE '$DB_NAME'");
-            if ($result->num_rows == 0) {
-                die("Por favor, crea la base de datos '$DB_NAME' ejecutabdo db_init.php");
-            }
-
-            // Seleccionar la base de datos
-            $conn->select_db($DB_NAME);
             ?>
             <div class="container" id="div_padre">
                 <?php
-                if (isset($_GET['id_imdb']) && $_GET['id_imdb'] != '') {
-                    /**
-                     * Create html for api 
-                     */
+                if ($view == 'search') {
                     if (isset($_POST['titulo']) && isset($_POST['desc']) && isset($_POST['anho']) && isset($_POST['duracion'])) {
                         $id_imdb = $_GET['id_imdb'];
                         $img_url = $_POST['imagen'];
@@ -64,32 +46,24 @@ require_once 'conf.php';
                 ?>
                         <input type="hidden" id="id_imdb" value="<?= $_GET['id_imdb'] ?>">
                         <script src="assets/js/imdb.js"></script>
-
                         <div class=" lg:py-12 lg:flex lg:justify-center text-lg">
                             <div class=" lg:w-full lg:flex lg:rounded-lg">
                                 <div class="lg:w-1/2 flex items-center justify-center">
                                     <img src="<?= $img_url ?>" alt="Descripción de la imagen" class="h-64 w-50  lg:h-full ">
                                 </div>
-
                                 <div class=" px-4 lg:w-1/2">
                                     <h2 class="text-3xl text-gray-800 font-bold"><?= $titulo ?></h2>
-
                                     <p class="movie-description  mt-4 text-gray-600"><span class="text-black">Sinopsis: </span> <span id="generos"><img src="assets/img/loading.gif" alt="" class="loading_gif"></span>
                                         <br><?= $desc ?>
                                     </p>
-
                                     <p class="mt-4 text-gray-600"><span class="text-black">Año: </span><?= $anho ?></p>
-
                                     <p class="mt-4 text-gray-600" id="p_duracion"><span class="text-black">Duración: </span><?= $duracion ?></p>
-
 
                                     <!-- Aquí va el rating: Esto se creará después de hacer el ajax -->
                                     <div class="rating_stars" id="div_rating_stars">
                                         <p id="p_rating" class="mt-4 text-gray-600 text-black">Rating: <img src="assets/img/loading.gif" alt="" class="loading_gif"></p>
 
                                     </div>
-
-
                                     <!-- FIN rating -->
 
                                     <!-- Aquí va los productores y compañías: Esto se creará después de hacer el ajax -->
@@ -101,10 +75,8 @@ require_once 'conf.php';
                                     <div class="mt-8">
                                         <a href="#" class="bg-gray-900 text-gray-100 px-5 py-3 font-semibold rounded">Start Now</a>
                                     </div>
-
                                 </div>
                             </div>
-
                         </div>
 
                         <div class="extra_info">
@@ -113,14 +85,9 @@ require_once 'conf.php';
                                 <h2 class="text-3xl text-gray-800 font-bold">Productoras</h2>
                                 <div id="div_company">
                                     <img src="assets/img/loading.gif" alt="" class="loading_gif">
-
-
                                 </div>
-
                             </div>
                             <br>
-
-
                             <div class="casting">
                                 <h2 class="text-3xl text-gray-800 font-bold">Casting</h2>
                                 <div id="div_casting" class=" dark:text-gray-400">
@@ -134,48 +101,45 @@ require_once 'conf.php';
                         echo "boton para volver atrás";
                     }
                 } else {
-                    $elementos = get_elements_sql($conn);
                     foreach ($elementos as $elemento) {
                     ?>
-                        <input type="hidden" id="id_imdb" value="<?= $elemento->getId_imdb() ?>">
+                        <input type="hidden" id="id_imdb" value="<?= $elemento->id ?>">
                         <!-- cellphone-container -->
-                        <div class="cellphone-container" id="cardbox_<?= $elemento->getId_imdb() ?>">
+                        <div class="cellphone-container" id="cardbox_<?= $elemento->id ?>">
 
                             <!-- movie-img -->
                             <div class="movie-img">
-                                <img src="<?= $elemento->getImg_url() ?>" class="img_portada" alt="portada elemento" style="border-top-left-radius:5px; border-top-right-radius : 5px">
+                                <img src="<?= $elemento->img_url ?>" class="img_portada" alt="portada elemento" style="border-top-left-radius:5px; border-top-right-radius : 5px">
                             </div>
                             <!-- FIN movie-img -->
 
                             <!-- movie-details -->
                             <div class="movie-details">
-                                <h2><?= $elemento->getTitulo() ?></h2>
-                                <p class="desc"><?= $elemento->getDescripcionFormatted($elemento->getDescripcion()) ?></p><br>
-                                <p class="tipo" id="tipo_<?= $elemento->getId_imdb() ?>"><i class="fa-solid fa-tag"></i>&nbsp&nbsp <?=$elemento->getTipo() == 'movie' ? 'Película' : (($elemento->getTipo() == 'serie') ? 'Serie' : 'N/A')?></p>
-                                <p class="anho" id="anho_<?= $elemento->getId_imdb() ?>"><i class="fa-solid fa-calendar-days"></i>&nbsp&nbsp<?= $elemento->getAnho() ?></p>
-                                <p class="duracion" id="duracion_<?= $elemento->getId_imdb() ?>"><i class="fa-solid fa-clock"></i>&nbsp&nbsp<?= $elemento->getDuracion() ?></p>
+                                <h2><?= $elemento->titulo ?></h2>
+                                <p class="desc"><?= $elemento->descripcion ?></p><br>
+                                <p class="tipo" id="tipo_<?= $elemento->id ?>"><i class="fa-solid fa-tag"></i> <?= $elemento->tipo == 'movie' ? 'Película' : (($elemento->tipo == 'serie') ? 'Serie' : 'N/A') ?></p>
+                                <p class="anho" id="anho_<?= $elemento->id ?>"><i class="fa-solid fa-calendar-days"></i><?= $elemento->anho ?></p>
+                                <p class="duracion" id="duracion_<?= $elemento->id ?>"><i class="fa-solid fa-clock"></i><?= $elemento->duracion ?></p>
                                 <br>
-                                <form action="?id_imdb=<?= $elemento->getId_imdb() ?>" method="POST">
-                                    <input type="hidden" name="id_imdb" value="<?= $elemento->getId_imdb() ?>" />
-                                    <input type="hidden" name="imagen" value="<?= $elemento->getImg_url() ?>" />
-                                    <input type="hidden" name="titulo" value="<?= $elemento->getTitulo() ?>" />
-                                    <input type="hidden" name="desc" value="<?= $elemento->getDescripcion() ?>" />
-                                    <input type="hidden" name="anho" value="<?= $elemento->getAnho() ?>" />
-                                    <input type="hidden" name="duracion" value="<?= $elemento->getDuracion() ?>" />
+                                <form action="?id_imdb=<?= $elemento->id ?>" method="POST">
+                                    <input type="hidden" name="id_imdb" value="<?= $elemento->id ?>" />
+                                    <input type="hidden" name="imagen" value="<?= $elemento->img_url ?>" />
+                                    <input type="hidden" name="titulo" value="<?= $elemento->titulo ?>" />
+                                    <input type="hidden" name="desc" value="<?= $elemento->descripcion ?>" />
+                                    <input type="hidden" name="anho" value="<?= $elemento->anho ?>" />
+                                    <input type="hidden" name="duracion" value="<?= $elemento->duracion ?>" />
                                     <button class="btn_action" id="btn_info">Ver más</button>
                                 </form>
                             </div>
                             <!-- FIN movie-details -->
-                             
+
                         </div><!-- FIN cellphone-container -->
                     <?php
                     }
                     ?>
                 <?php } ?>
             </div><!-- fin container class -->
-
         </section>
-
     </div>
     <?php
     include('footer.php')
