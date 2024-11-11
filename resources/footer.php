ba6e5f494cddbdead21d8755c86d37eca1c93ad9
ricardodev9 <164495350+ricardodev9@users.noreply.github.com>
2024-11-11 01:32:31 +0100

    Cambio de sitio del archivo footer

---
+++ resources/footer.php
@@ -0,0 +1,45 @@
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-body-tertiary text-muted">
+      <!-- Section: Social media -->
+      <section class="d-flex justify-content-center ">
+        <!-- Left -->
+        <div class="me-2 d-none d-lg-block ">
+          <span>Información de contacto:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div id="footer_content">
+          <a href="mailto:ricardoapazacueva2000@gmail.com" class="me-4 text-reset">
+            <i class="fab fa-google"></i>
+          </a>
+          <a href="tel:+34698109435" class="me-4 text-reset">
+            <i class="bi bi-telephone-forward-fill"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ricardo-apaza-cueva-43b0572a1" target="_blank" class="me-4 text-reset">
+            <i class="bi bi-linkedin"></i>
+          </a>
+          <a href="https://github.com/ricardodev9" target="_blank" class="me-4 text-reset">
+            <i class="bi bi-github"></i>
+          </a>
+        </div>
+        <!-- Right -->
+      </section>
+      <!-- Section: Social media -->
+
+      <!-- Section: Links  -->
+      <section class="">
+        <div class="container text-center text-md-start mt-5">
+
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2024 Copyright:
+        <a class="text-reset fw-bold" href="https://github.com/ricardodev9" target="_blank">Ricardodev9</a>
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
